143a764f9ed5d78d66d7092b6b9f481386596898
TomasMerva <tmerva7@gmail.com>
2024-08-27 11:56:56 +0200

    working with collision avoidance on real robot

--- fabrics_rollouts/__init__.py
+++ fabrics_rollouts/__init__.py
@@ -4,3 +4,4 @@ __version__ = version(__name__)
 from fabrics_rollouts.scripts.solver.grasp_sqp import GompSQP
 from fabrics_rollouts.scripts.cpp_bridge.fabrics_driver import FabricsDriver
 from fabrics_rollouts.scripts.rollout.fabrics_rollouts import RolloutFabrics
+from fabrics_rollouts.scripts.utils.reference_tracker import ReferenceTracker
