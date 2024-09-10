b1961a260fd06ec657b2902531c0295e469fe0d4
TomasMerva <tmerva7@gmail.com>
2024-09-10 11:56:25 +0200

    structure for examples without collisions

--- fabrics_rollouts/scripts/solver/grasp_sqp.py
+++ fabrics_rollouts/scripts/solver/grasp_sqp.py
@@ -81,12 +81,11 @@ class GompSQP():
         
     
         if np.isnan(result).any():
-            print("enne")
-            print("auch enne")
+            print("Enne? Auch enne")
             return (result.reshape((self._num_waypoints, self._num_dim)), "failed")
         else:
-            print("no limburgs")
-        return (result.reshape((self._num_waypoints, self._num_dim)), res.info.status)
+            # print("no limburgs")
+            return (result.reshape((self._num_waypoints, self._num_dim)), res.info.status)
 
    
     def _create_quadratic_objective_term(self, num_waypoints, num_dof):
