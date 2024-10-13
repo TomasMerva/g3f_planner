79f18d414d4da7c0caf35975f230b0c0d0ae2d9b
TomasMerva <tmerva7@gmail.com>
2024-10-13 21:18:02 +0200

    placing with crossover

--- examples/evaluation/dinovas_crossover/dinovas_pybullet_env.py
+++ examples/evaluation/dinovas_crossover/dinovas_pybullet_env.py
@@ -29,7 +29,7 @@ class Environment():
     def _define_files_path(self, env_config_file) -> None:
         current_script_dir = os.path.dirname(os.path.abspath(__file__))
     
-        config_path = os.path.join(current_script_dir, '../config', env_config_file)
+        config_path = os.path.join(current_script_dir, '../../../config', env_config_file)
         self.CONFIG_FILE = os.path.normpath(config_path)
 
         with open(self.CONFIG_FILE, 'r') as config_file:
@@ -37,7 +37,7 @@ class Environment():
             self.CONFIG_PROBLEM = self.CONFIG['problem']
             self.CONFIG_FABRICS = self.CONFIG['fabrics']
 
-        self.URDF_FOLDER = os.path.normpath( os.path.join(current_script_dir, 'urdfs'))
+        self.URDF_FOLDER = os.path.normpath( os.path.join(current_script_dir,  '../../urdfs'))
         self.ROBOT_URDF_FILE = self.URDF_FOLDER + self.CONFIG_PROBLEM["robot_representation"]["urdf_file"]
 
     def store_randomized_settings(self):
@@ -106,9 +106,6 @@ class Environment():
         
         if home_config is not None:
             self.home_config = home_config
-        # self.home_config[0][:3] = [1.22466862, -1.72433948,  1.94785656]
-        # self.home_config[1] = np.array([-0.188354, 0.84605903, -1.34784925, -0.01746023, -0.90232639,  1.16808536,  -0.05370079, -0.4273152,  -0.02838291, 0.9, -0.9])
-        # self.home_config[2] = np.array([-0.64654884, -0.54696915,  0.47987815,  0.00676067, -1.0327904,   0.94264399, -0.07074774,  0.16436266, -0.07603307, 0.9, -0.9])
 
         pos0 = self.home_config[0:nr_robots]
         self.env.reset(pos=pos0)
@@ -117,31 +114,7 @@ class Environment():
             self.env.add_obstacle(obst)
         self.env.set_spaces()
 
-        pybullet_links_idx = {
-            'world': -1, 
-            'base_link': 0, 
-            'base_link_x': 1, 
-            'base_link_y': 2, 
-            'chassis_link': 3, 
-            'front_left_wheel_link': 4,
-            'front_right_wheel_link': 5, 
-            'rear_left_wheel_link': 6, 
-            'rear_right_wheel_link': 7, 
-            'mid_mount': 8,
-            'arm_upper_wrist_link': 17, 
-            'arm_end_effector_link': 18, 
-            'arm_dummy_link': 19, 
-            'arm_tool_frame': 20, 
-            'arm_orientation_helper_link': 21, 
-            'arm_gripper_base_link': 22, 
-            'arm_right_finger_prox_link': 23, 
-            'arm_left_finger_prox_link': 24, 
-            'rear_c_mount': 25, 
-            'rear_b_mount': 26, 
-            'rear_mount': 27, 
-            'front_bumper_mount': 28
-            }
-        
+               
         self.collision_links = {}
         for i_robot in range(nr_robots):
             for link_name, val in self.CONFIG_PROBLEM["robot_representation"]["collision_links"].items():
@@ -163,75 +136,22 @@ class Environment():
             table_pos = self.table_pos
             z_table = self.z_table
             table_poses = self.table_poses
-
-            if self._objects_pose_noise is None:
-                objects_pos = [
-                    [table_poses[0][0] - 0.05, table_poses[0][1] + 0.1, z_table],
-                    [table_poses[1][0] + 0.05, table_poses[1][1] + 0.1, z_table],
-                    [table_poses[0][0] - 0.05, table_poses[0][1] - 0.1, z_table],
-                    [table_poses[1][0] + 0.05, table_poses[1][1] - 0.1, z_table],
-                ]
-            else:
-                objects_pos = [
-                    [table_poses[0][0] + self._objects_pose_noise[0][0], table_poses[0][1] - self._objects_pose_noise[0][1],
-                     z_table],
-                    [table_poses[1][0] + self._objects_pose_noise[1][0], table_poses[1][1] - self._objects_pose_noise[1][1],
-                     z_table],
-                    [table_poses[0][0] + self._objects_pose_noise[2][0], table_poses[0][1] - self._objects_pose_noise[2][1],
-                     z_table],
-                    [table_poses[1][0] + self._objects_pose_noise[3][0], table_poses[1][1] - self._objects_pose_noise[3][1],
-                     z_table],
-                ]
-        elif self.n_robots == 3:
-            URDF_table = self.URDF_FOLDER + "/table_50x50/table_square.urdf"
-            table_pos = self.table_pos
-            z_table = self.z_table
-            objects_pos = [
-                    [table_pos[0]+self._objects_pose_noise[0][0], table_pos[1]-self._objects_pose_noise[0][1], z_table],
-                    [table_pos[0]+self._objects_pose_noise[1][0], table_pos[1]-self._objects_pose_noise[1][1], z_table],
-                    [table_pos[0]+self._objects_pose_noise[2][0], table_pos[1]-self._objects_pose_noise[2][1], z_table],
-                    [table_pos[0]+self._objects_pose_noise[3][0], table_pos[1]-self._objects_pose_noise[3][1], z_table],
-                ]
-        else:
-            URDF_table = self.URDF_FOLDER + "/table_50x50/table_square.urdf"
-
-            table_pos = [0., 0.0, 0.0]
-            z_table = 0.3
-            
-            if self._objects_pose_noise is None:
-                objects_pos = [
-                    [table_pos[0]-0.05, table_pos[1]+0.1, z_table],
-                    [table_pos[0]+0.05, table_pos[1]+0.1, z_table],
-                    [table_pos[0]-0.05, table_pos[1]-0.1, z_table],
-                    [table_pos[0]+0.05, table_pos[1]-0.1, z_table],
-                ]
-            else:
-                objects_pos = [
-                    [table_pos[0]+self._objects_pose_noise[0][0], table_pos[1]-self._objects_pose_noise[0][1], z_table],
-                    [table_pos[0]+self._objects_pose_noise[1][0], table_pos[1]-self._objects_pose_noise[1][1], z_table],
-                    [table_pos[0]+self._objects_pose_noise[2][0], table_pos[1]-self._objects_pose_noise[2][1], z_table],
-                    [table_pos[0]+self._objects_pose_noise[3][0], table_pos[1]-self._objects_pose_noise[3][1], z_table],
-                ]
-
+        
         self.scene_id = {}
-        for object_id in range(self.n_robots):
-            urdf_file = self.URDF_FOLDER + "/cup/cup_" + str(object_id+1) +".urdf"
-            object_pybulletID = pybullet.loadURDF(urdf_file, basePosition=objects_pos[object_id])
-            self.scene_id["cup_"+str(object_id)] = object_pybulletID
-        if self.nr_tables <= 1:
-            self.scene_id["table"] = pybullet.loadURDF(URDF_table, basePosition=table_pos, globalScaling=1)
-        else:
-            for i_table in range(self.nr_tables):
-                self.scene_id["table"] = pybullet.loadURDF(URDF_table, basePosition=table_poses[i_table],
-                                                           globalScaling=1)
+
+    
+        
+        for i_table in range(self.nr_tables):
+            self.scene_id["table"] = pybullet.loadURDF(URDF_table, basePosition=table_poses[i_table],
+                                                    globalScaling=1)
+            self.scene_id["cup_"+str(i_table)] = [table_poses[i_table][0],
+                                                  table_poses[i_table][1],
+                                                  z_table+0.05]
 
     def get_config_file_path(self):
         return self.CONFIG_FILE
     
-    def get_cup(self, cup_id):
-        name = "cup_" + str(cup_id)
-        return pybullet.getBasePositionAndOrientation(self.scene_id[name])
-    
+      
     def get_table_pose(self):
         return pybullet.getBasePositionAndOrientation(self.scene_id["table"])
     
@@ -245,6 +165,10 @@ class Environment():
     def set_objects_pos_noise(self, pos):
         assert self._objects_pose_noise == None, "Objects positions have already been set"
         self._objects_pose_noise = pos
+
+    def get_cup(self, cup_id):
+        print(self.scene_id["cup_"+str(cup_id)])
+        return (self.scene_id["cup_"+str(cup_id)], [0,0,0,1])
 
     def reset(self):
         pos0 = self.home_config[0:self.n_robots]
@@ -270,8 +194,6 @@ class Environment():
     
     def get_object_pose(self):
         x = []
-        for i in range(self.n_robots):
-            x.append(self.get_cup(i))
         return x
     
 
