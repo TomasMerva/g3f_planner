11316923e8350a0db844967c55e97b55bc62a17c
Saray <saraybakker@gmail.com>
2024-10-18 14:56:12 +0200

    3robot evaluation for RGF working

--- examples/evaluation/dinovas_3robots/example_rgf_dinovas.py
+++ examples/evaluation/dinovas_3robots/example_rgf_dinovas.py
@@ -81,6 +81,8 @@ def run_dinova_example(n_steps,
         config_path = os.path.join(current_script_dir, 'dinova_config_rgf_5obst.yaml')
     elif NUM_OBST == 6:
         config_path = os.path.join(current_script_dir, 'dinova_config_rgf_6obst.yaml')
+    elif NUM_OBST == 9:
+        config_path = os.path.join(current_script_dir, 'dinova_config_rgf_9obst.yaml')
     CONFIG_FILE_PATH_GOMP = os.path.normpath(config_path)
     planner = RGF_Planner(fk_args=fk_args,
                           config_file_path=CONFIG_FILE_PATH_GOMP
